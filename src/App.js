ec1770275fd98ec03232e2b0bc90bdb5173b9a13
adsonbmx15 <adsonbmx18@gmail.com>
2022-07-05 14:55:48 -0300

    Projeto Dogs App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            {/* <Route path="conta/*" element={<User />} /> */}
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
