dd40b89acba25fe463accbf3166981905ee6914f
adsonbmx15 <adsonbmx18@gmail.com>
2022-07-07 23:14:41 -0300

    Graph Stats

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { STATS_GET } from '../../Api';
+import useFetch from '../../Hooks/useFetch';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
